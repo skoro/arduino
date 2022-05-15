9dbd99cdc8dac18445826b6b5bd978dd171531d0
Alexei Skorobogatko <skorobogatko.oleksii@gmail.com>
2022-05-15 19:29:08 +0300

    Comments

--- dht11/dht11.ino
+++ dht11/dht11.ino
@@ -1,7 +1,13 @@
+// Library https://github.com/johnrickman/LiquidCrystal_I2C
 #include <LiquidCrystal_I2C.h>
+
 #include <DFRobot_DHT11.h>
 
 DFRobot_DHT11 DHT;
+
+// I2C address pool for chips:
+// PCF8574T - 0x20-0x27
+// CF8574AT - 0x38-0x3f
 LiquidCrystal_I2C lcd(0x3f, 16, 2);
 
 #define DHT11_PIN 4
